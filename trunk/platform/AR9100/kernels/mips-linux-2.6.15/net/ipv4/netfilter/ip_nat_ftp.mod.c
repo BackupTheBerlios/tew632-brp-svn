7dbf529706a3abeda89ff241adbe41384391ec2d
jcollake <jcollake@54fdfdf6-d254-0410-a6db-dc2cac267283>
2008-08-20 18:46:27 +0000

    more fixes for lost files in vendor pkzip distrib

---
+++ trunk/platform/AR9100/kernels/mips-linux-2.6.15/net/ipv4/netfilter/ip_nat_ftp.mod.c
@@ -0,0 +1,27 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+#undef unix
+struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+ .name = __stringify(KBUILD_MODNAME),
+ .init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+ .exit = cleanup_module,
+#endif
+};
+
+static const struct modversion_info ____versions[]
+__attribute_used__
+__attribute__((section("__versions"))) = {
+	{ 0xf4e79203, "ip_nat_ftp_hook" },
+};
+
+static const char __module_depends[]
+__attribute_used__
+__attribute__((section(".modinfo"))) =
+"depends=ip_conntrack_ftp";
+
